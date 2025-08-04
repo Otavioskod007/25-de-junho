c0856906d393e36e7da514fe6fa0e31466329092
Otavioskod007 <167528214+Otavioskod007@users.noreply.github.com>
2025-08-04 12:21:39 -0300

    site2025

---
+++ .js
@@ -0,0 +1,16 @@
+
+const perguntas = [
+    {
+        enunciado: Qual a maior carta do truco, sem ser a manilha ?,
+        alternativas: [
+            { texto: 3,
+                afirmação: resposta correta,
+                pontuação: 1
+            },
+            { texto: 4,
+                afirmação: resposta errada,
+                pontuação 0,
+            }
+        ]
+    }
+]
